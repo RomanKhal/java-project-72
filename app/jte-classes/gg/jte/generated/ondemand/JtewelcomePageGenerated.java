8a328fbd8ca07b0f95867f280892aa8f576a1e73
RomanKhal <haluev.roman@mail.ru>
2024-05-19 15:27:37 +0300

    4d step

---
+++ app/jte-classes/gg/jte/generated/ondemand/JtewelcomePageGenerated.java
@@ -0,0 +1,15 @@
+package gg.jte.generated.ondemand;
+public final class JtewelcomePageGenerated {
+	public static final String JTE_NAME = "welcomePage.jte";
+	public static final int[] JTE_LINE_INFO = {0,0,0,0,0,2,2,6,6,6,6,6,6,6,6};
+	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor) {
+		gg.jte.generated.ondemand.layout.JtepageGenerated.render(jteOutput, jteHtmlInterceptor, new gg.jte.html.HtmlContent() {
+			public void writeTo(gg.jte.html.HtmlTemplateOutput jteOutput) {
+				jteOutput.writeContent("\n    <div class=\"mx-auto p-4 py-md-5\">\n        Welcome PAGE\n    </div>\n");
+			}
+		});
+	}
+	public static void renderMap(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, java.util.Map<String, Object> params) {
+		render(jteOutput, jteHtmlInterceptor);
+	}
+}
