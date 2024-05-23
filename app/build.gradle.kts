54ad898297e0f027528616cea8cbbd583b83ce16
RomanKhal <haluev.roman@mail.ru>
2024-05-23 20:08:26 +0300

    5d step

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
     implementation("org.slf4j:slf4j-simple:2.0.12")
     implementation("com.zaxxer:HikariCP:5.1.0")
     implementation("com.h2database:h2:2.2.224")
+    implementation("org.postgresql:postgresql:42.1.2")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
     implementation("gg.jte:jte:3.1.11")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -18,6 +18,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.sql.Driver;
 import java.sql.SQLException;
 import java.util.stream.Collectors;
 
@@ -33,6 +34,10 @@ public class App {
     private static int getPort() {
         String port = System.getenv().getOrDefault("PORT", "7070");
         return Integer.parseInt(port);
+    }
+
+    private static String getRemoteDB(){
+       return System.getenv().getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:project");
     }
 
     private static TemplateEngine createTemplateEngine() {
@@ -51,7 +56,8 @@ public class App {
 
     public static Javalin getApp() throws IOException, SQLException {
         HikariConfig hikariConfig = new HikariConfig();
-        hikariConfig.setJdbcUrl(System.getenv().getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:project;DB_ClOSE_DELAY=-1;"));
+        hikariConfig.setJdbcUrl(getRemoteDB());
+
 
         var dataSource = new HikariDataSource(hikariConfig);
         var sql = readResourceFile("schema.sql");
