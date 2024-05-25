0edcf70c2ade062660276ed38e24347e7b1c01b5
RomanKhal <haluev.roman@mail.ru>
2024-05-25 17:15:08 +0300

    5d step

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     application
+    jacoco
     id("io.freefair.lombok") version "8.6"
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
@@ -17,13 +18,15 @@ repositories {
 
 dependencies {
     implementation("io.javalin:javalin:6.1.3")
+    implementation("io.javalin:javalin-bundle:6.1.3")
     implementation("io.javalin:javalin-rendering:6.1.3")
-    implementation("org.slf4j:slf4j-simple:2.0.12")
     implementation("com.zaxxer:HikariCP:5.1.0")
     implementation("com.h2database:h2:2.2.224")
     implementation("org.postgresql:postgresql:42.1.2")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
     implementation("gg.jte:jte:3.1.11")
+
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
---
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -0,0 +1,28 @@
+package hexlet.code;
+
+import hexlet.code.util.NamedRoutes;
+import io.javalin.Javalin;
+import io.javalin.testtools.JavalinTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class AppTest {
+    Javalin app;
+
+    @BeforeEach
+    public final void setApp() throws SQLException, IOException {
+        app = App.getApp();
+    }
+
+    @Test
+    public void testMainPage() {
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get(NamedRoutes.home());
+            Assertions.assertEquals(response.code(), 200);
+        });
+    }
+}
